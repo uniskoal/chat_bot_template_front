5169d6ef6f0f5d51e979c06f9d5ac9df9949788c
김준서 <uniskoal036@gmail.com>
2024-05-19 13:47:01 +0900

    Test: webpack 프록시 설정

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,13 @@ module.exports = {
             directory: path.join(__dirname , 'public'),
         },
         compress: true,
-        port: 3000
+        port: 3000,
+        proxy: {
+            '**': {
+                target: 'http://127.0.0.1:8000',
+                changeOrigin: true,
+            },
+        },
     },
 
     module: {
